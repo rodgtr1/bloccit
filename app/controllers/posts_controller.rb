19ca350e776a619c4dcbadb9b6e7e8cd04578564
Travis Rodgers <rodgtr1@gmail.com>
2017-03-05 21:43:09 -0500

    CR in CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,27 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
+
+  def create
+    #creates new instance of post
+     @post = Post.new
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+
+     if @post.save
+       flash[:notice] = "Post was saved."
+       redirect_to @post
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :new
+     end
+   end
 
   def edit
   end
