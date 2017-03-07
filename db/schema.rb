63cb349d0f5ca30f87dc6b763ea334898a71c332
Travis Rodgers <rodgtr1@gmail.com>
2017-03-06 20:19:00 -0500

    added Advertisements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303032013) do
+ActiveRecord::Schema.define(version: 20170306025534) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
